276d0c319fbfd0894f62cc0b7862a5c38f91a5c5
Ash Isbitt <AIsbitt@tcadmins-MacBook-Air-2.local>
2020-03-03 12:22:33 +0000

    start of integers and boolean files

---
+++ 103_integers.py
@@ -0,0 +1,15 @@
+# Integers and floats
+
+#int
+print(10)
+print(type(10))
+
+#float
+print(10.0)
+print(type(10.0))
+
+print(2 + 2)
+print(5 - 2)
+print(2 * 2)
+print(6 / 2)
+print(5 % 2)
---
+++ 104_boolean.py
@@ -0,0 +1,15 @@
+#boolean
+# true or false
+
+print(True)
+print(False)
+print(type(True))
+print(type(False))
+
+print(3 == 5)
+print(3 > 5)
+print(3 < 5)
+print(3 >= 5)
+print(3 <= 5)
+print(3 != 5)
+
